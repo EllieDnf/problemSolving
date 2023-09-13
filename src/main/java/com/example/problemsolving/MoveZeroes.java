1bd15e362af5a54b6036f05ed20f8c40d83f232a
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2023-09-13 10:05:36 -0400

    problem solved

---
+++ src/main/java/com/example/problemsolving/MoveZeroes.java
@@ -0,0 +1,28 @@
+package com.example.problemsolving;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        int[] exp = new int[]{1,1,0,3,12,0};
+        moveZeroes(exp);
+        System.out.println(Arrays.toString(exp));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        for (int i=0; i<nums.length;i++){
+            if (nums[i]==0){
+                for (int j=i+1; j<nums.length;j++) {
+                    if (nums[j] != 0) {
+                        nums[i] = nums[j];
+                        i++;
+                    }
+                }
+                while(i<nums.length) {
+                    nums[i]=0;
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/problemsolving/TwoSum.java
@@ -0,0 +1,5 @@
+package com.example.problemsolving;
+
+public class TwoSum {
+
+}
