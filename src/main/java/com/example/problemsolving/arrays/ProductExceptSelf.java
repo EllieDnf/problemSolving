0d3cb12a3aaf3af8e4a3c38cc46d3fa998b25884
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2023-09-27 23:22:58 -0400

    3 new problem solved

--- src/main/java/com/example/problemsolving/arrays/ProductExceptSelf.java
+++ src/main/java/com/example/problemsolving/arrays/ProductExceptSelf.java
@@ -4,43 +4,44 @@ import java.util.Arrays;
 
 public class ProductExceptSelf {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(productExceptSelf(new int[]{1,2,3,4})));
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
     }
+
     public static int[] productExceptSelf(int[] nums) {
-        int[]ans= new int[nums.length];
-        int countZero=0;
-        int idxZero=-1;
-        for (int i=0; i<ans.length;i++){
-            if (nums[i]==0) {
+        int[] ans = new int[nums.length];
+        int countZero = 0;
+        int idxZero = -1;
+        for (int i = 0; i < ans.length; i++) {
+            if (nums[i] == 0) {
                 countZero++;
-                idxZero=i;
+                idxZero = i;
             }
-            if (countZero>1) return ans;
+            if (countZero > 1) return ans;
         }
-        if(countZero==1) {
-            ans[idxZero] =productExceptIdx(nums,idxZero);
+        if (countZero == 1) {
+            ans[idxZero] = productExceptIdx(nums, idxZero);
             return ans;
         }
-        for(int i=0; i<nums.length;i++){
-            ans[i]= product(nums)/nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = product(nums) / nums[i];
         }
         return ans;
     }
 
-    public static int product (int[] nums){
-        int product=1;
-        for (int i=0; i<nums.length ;i++){
-            product=product*nums[i];
+    public static int product(int[] nums) {
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            product = product * nums[i];
         }
         return product;
     }
 
-    public static int productExceptIdx (int[] nums, int idx){
-        int product=1;
-        for (int i=0; i<nums.length ;i++){
-            if (i==idx && i!=nums.length-1) i++;
-            if (i==idx && i==nums.length-1) return product;
-            product=product*nums[i];
+    public static int productExceptIdx(int[] nums, int idx) {
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == idx && i != nums.length - 1) i++;
+            if (i == idx && i == nums.length - 1) return product;
+            product = product * nums[i];
         }
         return product;
     }
