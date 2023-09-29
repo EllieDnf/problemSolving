47f63ecddc28be18c7a7c759e681a9b6d17f0482
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2023-09-29 09:48:20 -0400

    product Except itSelf solved

--- src/main/java/com/example/problemsolving/arrays/ProductExceptSelf.java
+++ src/main/java/com/example/problemsolving/arrays/ProductExceptSelf.java
@@ -6,7 +6,6 @@ public class ProductExceptSelf {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
     }
-
     public static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
