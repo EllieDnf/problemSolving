cb8f0111db63d4562fcde9e5355a8a991377d996
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2023-09-29 13:32:32 -0400

    Three Sum solved

---
+++ src/main/java/com/example/problemsolving/TwoPointers/ThreeSum.java
@@ -0,0 +1,33 @@
+package com.example.problemsolving.TwoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -1, -4}));
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triples = new ArrayList<>();
+        int len = nums.length;
+        for (int i = 0; i < len - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triples.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum > 0) right--;
+                else left++;
+            }
+        }
+        return triples;
+    }
+}
