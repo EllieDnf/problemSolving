d8894ce7c655cfb2de204f1761d7cc0a2490f4fb
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2023-09-29 10:45:05 -0400

    Valid Palindrome solved

---
+++ src/main/java/com/example/problemsolving/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,36 @@
+package com.example.problemsolving.TwoPointers;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("anna"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        StringBuilder cleanString = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleanString.append(c);
+            }
+        }
+
+        s = cleanString.toString();
+        cleanString = cleanString.reverse();
+        String cleaned = cleanString.toString();
+        return s.equals(cleaned);
+    }
+    public static boolean isPalindrome2(String s) {
+        String a = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int i=0;
+        int j=a.length()-1;
+
+        while(i<=j){
+            if (a.charAt(i) != a.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
