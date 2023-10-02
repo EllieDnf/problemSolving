4d655d568d560e291e2a25924310a398bee4962a
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2023-10-02 13:05:00 -0400

    Evaluate Reverse Polish Notation solved

---
+++ src/main/java/com/example/problemsolving/Stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,25 @@
+package com.example.problemsolving.Stack;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    public static void main(String[] args) {
+        System.out.println(evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
+    }
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> nums = new Stack<>();
+        for (String s: tokens) {
+            if (!s.equals("+") && !s.equals("-") && !s.equals("*") && !s.equals("/")) nums.add(Integer.parseInt(s));
+            else {
+                int first = nums.pop();
+                int sec = nums.pop();
+                if (s.equals("+")) nums.push(first+sec);
+                if (s.equals("-")) nums.push(sec-first);
+                if (s.equals("*")) nums.push(first*sec);
+                if (s.equals("/")) nums.push(sec/first);
+            }
+        }
+        return nums.pop();
+    }
+}
